57c504fb0199ce5d95f7a4d86484683f7701ff03
Shreyam Kundu <shreyamkundu04@gmail.com>
2024-11-28 13:10:49 +0530

    patch method added in cors

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(cors({
   origin: process.env.CLIENT_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }));
 
