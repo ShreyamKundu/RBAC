d2d1aa5559d1736d59860834d089bb09ce7ef26a
Shreyam Kundu <shreyamkundu04@gmail.com>
2024-11-28 14:05:08 +0530

    google oauth fix

--- backend/routes/oauth.route.js
+++ backend/routes/oauth.route.js
@@ -21,6 +21,7 @@ router.get(
 // Route to handle login success
 router.get("/login/success", async (req, res) => {
   res.setHeader("Cache-Control", "no-store");
+  console.log("req.user", req.user);
   if (req.user) {
     return res.status(200).json({
       success: true,
